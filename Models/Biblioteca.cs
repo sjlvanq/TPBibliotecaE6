4c5f313e36f9fe0a2a33c87bbad7c6def4bb2c74
sjlvanq <sjlvanq@gmail.com>
2026-09-13 20:48:08 -0300

    feat: add Biblioteca and Libro domain models

---
+++ Models/Biblioteca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPBibliotecaE6.Models
+{
+    public class Biblioteca
+    {
+        private List<Libro> libros;
+
+        public Biblioteca()
+        {
+            this.libros = new List<Libro>();
+        }
+        public Libro agregarLibro(string titulo)
+        {
+            Libro libro = new Libro(titulo);
+            this.libros.Add(libro);
+            return libro;
+        }
+        public List<Libro> obtenerLibros()
+        {
+            return this.libros;
+        }
+    }
+}
---
+++ Models/Libro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPBibliotecaE6.Models
+{
+    public class Libro
+    {
+        //private int id;
+        private string titulo;
+        public Libro(string titulo) {
+            this.titulo = titulo;
+        }
+        public override string ToString()
+        {
+            return this.titulo;
+        }
+    }
+}
