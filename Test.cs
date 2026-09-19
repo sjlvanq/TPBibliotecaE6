f59b6c11ac99ad7a6411c4e78d524b25a494c883
sjlvanq <sjlvanq@gmail.com>
2026-09-19 18:11:20 -0300

    test: add Lector tests

--- Test.cs
+++ Test.cs
@@ -14,8 +14,23 @@ namespace TPBibliotecaE6
             biblioteca.listarLibros();
             biblioteca.eliminarLibro("Libro 2");
             biblioteca.listarLibros();
+
+            Console.WriteLine("--------------------------------");
+
+            cargarLectores(10);
+            cargarLectores(2);
+            //biblioteca.listarLectores(); // No requerido; no implementado
+            prestarLibro("DNI0", "LIBRO_INEXISTENTE"); // Output esperado: "LIBRO INEXISTENTE"
+            prestarLibro("DNI_LECTOR_INEXISTENTE", "Libro 1"); // Output esperado: "LECTOR INEXISTENTE"
+            prestarLibro("DNI0", "Libro 1"); // Output esperado: "PRESTAMO REALIZADO CON ÉXITO"
+            // Libro 2 eliminado en línea 15
+            prestarLibro("DNI0", "Libro 3"); // Output esperado: "PRESTAMO REALIZADO CON ÉXITO"
+            prestarLibro("DNI0", "Libro 4"); // Output esperado: "PRESTAMO REALIZADO CON ÉXITO"
+            prestarLibro("DNI0", "Libro 5"); // Output esperado: "TOPE DE PRÉSTAMOS ALCANZADO"
+
             void cargarLibros(int cantidad)
             {
+                // Copiado literal de la bibliografía
                 bool pude;
                 for (int i = 0; i < cantidad; i++)
                 {
@@ -25,6 +40,26 @@ namespace TPBibliotecaE6
                     else
                         Console.WriteLine("libro " + i + " ya existe en la biblioteca.");
                 }
+            }
+
+            void cargarLectores(int cantidad)
+            {
+                bool pude;
+                for (int i = 0; i < cantidad; i++)
+                {
+                    pude = biblioteca.altaLector("Nombre " + i, "Apellido " + i, "DNI" + i);
+                    if (pude)
+                        Console.WriteLine("lector " + i + " agregado correctamente.");
+                    else
+                        Console.WriteLine("lector " + i + " ya existe en la biblioteca.");
+                }
+            }
+
+            void prestarLibro(string dni, string titulo)
+            {
+                string resultado;
+                resultado = biblioteca.prestarLibro(dni, titulo);
+                Console.WriteLine(resultado);
             }
         }
     }
